b5b9b1e0931ac1d18cce05e718d50cc5ac9ae453
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-09-13 01:03:42 -0400

    check signup + login routes are working and defined helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
-  get '/login' do
-    erb :login
+  # if the user is already signed up, redirect to reviews
+  # if the user is not signed up- sign up, direct to user form
+  # use helper methods
+
+  get '/signup' do
+    erb :'users/signup'
   end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
 
 end
