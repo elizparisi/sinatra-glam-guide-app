0f3c8b318c1c8b69252d4afdaf883e6c5e7379f8
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-09-14 16:25:03 -0400

    added routes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,69 @@
 class UsersController < ApplicationController
+  # test signup and login routes
+  #get '/signup' do
+  #  erb :'users/signup'
+  #end
 
-  # if the user is already signed up, redirect to reviews
-  # if the user is not signed up- sign up, direct to user form
-  # use helper methods
+  #get '/login' do
+  #  erb :'users/login'
+  #end
 
   get '/signup' do
-    erb :'users/signup'
+  # if the user is already signed up, redirect to products
+  # if the user is not signed up, they must sign up- direct to signup form
+  # user helper methods
+
+    if logged_in?
+      redirect '/products'
+
+    else
+      erb :signup
+    end
+  end
+
+  post '/signup' do
+  # creates a new user if all credentials are provided (email and password)
+  # if credentials are blank, redirect to signup
+
+    if params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+
+    else
+      @user = User.new(params)
+      @user.save
+      session[:user_id] = @user.id
+
+      redirect '/products'
+    end
   end
 
   get '/login' do
-    erb :'users/login'
+  # if a user is already logged in, they will be redirected to products page
+  # if the user is not logged in, send them to the login page (maybe user an error message here)
+
+    if logged_in?
+      redirect '/products'
+
+    else
+      erb :login
+    end
   end
+
+  #post login
+
+  #user show route
+
+  get '/logout' do
+
+    if logged_in?
+      session.clear
+
+      redirect '/login'
+
+    else
+      redirect '/'
+    end
+  end 
 
 
 end
