044ae4d0e9c27475d18cad7af89797af18572f2d
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-09-28 23:34:22 -0400

    added routes to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,24 @@
 class ReviewsController < ApplicationController
 
+  get '/reviews/new' do
+
+    erb :'/reviews new'
+  end
+
+  post '/reviews' do
+    @product = Product.new(name: params[:product][:name], description: params[:product][:description], image_url: params[:product][:image_url], rating: params[:product][:rating])
+
+    product.user = current_user
+
+    params[:product][:reviews].each do |review_info|
+      review = Review.new(review_info)
+      review.user = current_user
+      review.product = product
+      review.save
+    end
+
+    product.save
+
+    redirect '/products'
+  end 
 end
