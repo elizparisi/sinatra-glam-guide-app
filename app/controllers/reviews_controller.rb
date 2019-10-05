18210c60f6bff3a446b688a20b3a455065546082
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-10-05 00:55:39 -0400

    edited reviews controller + new review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,14 @@
 class ReviewsController < ApplicationController
 
+  # render reviews form
   get '/reviews/new' do
 
     erb :'/reviews/new'
   end
 
   post '/reviews' do
-    product = Product.new(name: params[:product][:name], description: params[:product][:description], image_url: params[:product][:image_url], rating: params[:product][:rating])
+    review = Review.create(content: params[:content], product_id: params[:product_id], user_id: session[:user_id])
 
-    product.user = current_user
-
-    params[:product][:reviews].each do |review_info|
-      review = Review.new(review_info)
-      review.user = current_user
-      review.product = product
-      review.save
-    end
-
-    product.save
-
-    redirect '/products'
-  end
+    redirect "/products/#{review.product_id}"
+  end 
 end
