7bd6149a1d7166c4c94ff10a15cb5e0c4c611d2f
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-10-05 11:11:26 -0400

    added review data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@ Product.create(name: "Paula's Choice 2% BHA Liquid Exfoliant", image_url: "https
 Product.create(name: "Glossier Generation G Lipstick", image_url: "https://images.ctfassets.net/p3w8f4svwgcg/5fvl7va5eRy41Lh264z5ay/cdc11be1f043cd64092089f22dd72092/GenG_HP_2.jpg", rating: 5, description: "Sheer, matte, blotted lipstick without the blot. Buildable color.", user_id: fiona.id)
 
 Product.create(name: "Maybelline Superstay Matte Ink Liquid Lipstick", image_url: "https://cdn-img.prettylittlething.com/c/7/a/b/c7abd20b885b630ffdd6853c6ceb728397b3539c_CLW4737_1.JPG", rating: 4, description: "Intensely pigmented, 16 hr wear, matte liquid lipstick. Doe foot applicator. Non-drying formula.", user_id: fiona.id)
+
+Review.create(content: "I really love these liquid lipsticks! They go on creamy and dry matte. Once they're dry they don't budge. It won't come off on clothes, even a napkin. I have 3 colors- a red, a pink and a neutral and love them all. I highly recommend these if you like the matte look.", product_id: 6, user_id: fiona.id)
